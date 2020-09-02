6a4887acc09c7ecf5936eefd919d29a158f31e6a
Matthew <mateuszzakrzewskipl@gmail.com>
2020-09-02 15:40:02 +0200

    Padding added and text moved to the right.

--- distribution/js/main.js
+++ distribution/js/main.js
@@ -32,42 +32,50 @@ function toggleMenu() {
     }
 }
 
-// // Multilanguage
-// const langEl = document.querySelector('.langWrap');
-// const link = document.querySelectorAll('.langWrap>a');
-// const headingEl = document.querySelector('.heading');
-// const btnEl = document.querySelector('.btn')
-// const aboutEl = document.querySelector('.aboutHeading');
-// const socialEl =  document.querySelector('.socialMedia');
-//
-//
-// link.forEach(el =>{
-//     el.addEventListener('click',() => {
-//         langEl.querySelector('.active').classList.remove('active');
-//         el.classList.add('active');
-//
-//
-//         const attr = el.getAttribute('lang');
-//
-//         headingEl.textContent = headingText[attr].heading;
-//         btnEl.textContent = headingText[attr].btn
-//         aboutEl.textContent = headingText[attr].aboutHeading;
-//         socialEl.textContent = headingText[attr].socialMedia;
-//
-//     });
-// });
-//
-// var headingText = {
-//     'english':{
-//         'socialMedia':'Follow Me On Social Media!',
-//         'aboutHeading': 'Full screen video landing page',
-//         'btn': 'Read More',
-//         'heading': 'Shoot for the moon. Even if you miss, you will land among the stars.'
-//     },
-//     'polish':{
-//         'socialMedia': 'Śledź Mnie W Mediach Społecznościowych!',
-//         'aboutHeading': 'Strona główna z pełnoekranowym plikiem video',
-//         'btn': 'Czytaj Więcej',
-//         'heading': 'Celuj w księżyc, bo nawet jeśli nie trafisz, będziesz między gwiazdami.'
-//     }
-// }
+// Multilanguage
+const langEl = document.querySelector('.langWrap');
+const link = document.querySelectorAll('.langWrap>a');
+const smHeadingEl = document.querySelector('.smHeading');
+const navLinkHomeEl = document.getElementById('navLinkHome');
+const navLinkAboutEl = document.getElementById('navLinkAbout');
+const navLinkWorkEl = document.getElementById('navLinkWork');
+const navLinkConEl = document.getElementById('navLinkCont');
+
+
+
+
+link.forEach(el =>{
+    el.addEventListener('click',() => {
+        langEl.querySelector('.active').classList.remove('active');
+        el.classList.add('active');
+
+
+        const attr = el.getAttribute('lang');
+
+        smHeadingEl.textContent = headingText[attr].smHeading;
+        navLinkHomeEl.textContent = headingText[attr].navLinkHome;
+        navLinkAboutEl.textContent = headingText[attr].navLinkAbout;
+        navLinkWorkEl.textContent = headingText[attr].navLinkWork;
+        navLinkConEl.textContent = headingText[attr].navLinkCont;
+
+
+
+    });
+});
+var headingText = {
+    'en':{
+        'smHeading': 'Web Developer, Programmer, Designer & Entrepreneur',
+        'navLinkHome': 'Home',
+        'navLinkAbout': 'About',
+        'navLinkWork': 'My Work',
+        'navLinkCont': 'Contact Me'
+
+    },
+    'pl':{
+        'smHeading': 'Programista Stron Internetowych, Programista, Projektant & Przedsiębiorca',
+        'navLinkHome': 'Strona Główna',
+        'navLinkAbout': 'O Mnie',
+        'navLinkWork': 'Moja Praca',
+        'navLinkCont': 'Nawiąż Kontakt Ze mną',
+    }
+}
